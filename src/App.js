cfac3e7445e8531cd5b809000a0f63ed21b57e21
Locktix <alanplokain@hotmail.com>
2025-07-15 01:47:26 +0200

    caca

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import VaccinsPage from "./pages/VaccinsPage";
